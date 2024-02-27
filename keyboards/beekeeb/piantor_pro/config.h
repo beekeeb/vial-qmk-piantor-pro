5b6e44c2f5575e96b8c6d3c63b8fa6bd3f7283ac
Leo Lou <louyuhong@gmail.com>
2024-02-27 17:02:46 +0800

    Add piantor pro
    Signed-off-by: Leo Lou <louyuhong@gmail.com>

---
+++ keyboards/beekeeb/piantor_pro/config.h
@@ -0,0 +1,33 @@
+// Copyright 2023 beekeeb (@beekeeb)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U // Timeout window in ms in which the double tap can occur.
+
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+#define SERIAL_USART_PIN_SWAP
+
+#define USB_VBUS_PIN GP19
+
+#define SPLIT_HAND_PIN GP17
+#define SPLIT_HAND_PIN_LOW_IS_LEFT
